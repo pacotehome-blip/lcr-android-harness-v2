1919bfa33400dc0356681067ab74fcc5d1700176
Côté <pacotehome@gmail.com>
2026-02-20 11:59:45 -0500

    APK payload v4

--- app/src/main/java/com/pa/lcrdemo/UsbReceiver.java
+++ app/src/main/java/com/pa/lcrdemo/UsbReceiver.java
@@ -5,6 +5,7 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.hardware.usb.*;
+import android.app.PendingIntent;
 
 import com.hoho.android.usbserial.driver.*;
 
