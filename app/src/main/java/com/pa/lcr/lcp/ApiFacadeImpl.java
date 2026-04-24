77d351355fe5c8e8150adcff4fa2ea750180fdf6
pacotehome-blip <pacotehome@gmail.com>
2026-04-24 14:01:17 -0400

    Update ApiFacadeImpl.javav2

--- app/src/main/java/com/pa/lcr/lcp/ApiFacadeImpl.java
+++ app/src/main/java/com/pa/lcr/lcp/ApiFacadeImpl.java
@@ -82,10 +82,13 @@ public final class ApiFacadeImpl implements ApiFacade {
     // LCP CONNECT
     // =========================================================
 
-    @Override
-    public ApiResult api_registerConnectAuto(String serialId, Integer lcrnode) {
-        return ApiResult.fail("Not supported", "NOT_SUPPORTED");
-    }
+@Override
+public ApiResult api_registerConnectAuto(String serialId, Integer lcrnode) {
+    return ApiResult.fail(
+        "registerConnectAuto: 0 - Not supported (mono-registre)",
+        "NOT_SUPPORTED"
+    );
+}
 
     @Override
     public ApiResult api_connectLcp() {
